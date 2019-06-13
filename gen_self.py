a93326c666299616fc1a9ee024a8ef14c0454038
Vincent Hui <vhui@alumni.ubc.ca>
2019-06-13 13:45:17 -0700

    back to self merging

--- gen_self.py
+++ gen_self.py
@@ -35,18 +35,6 @@ class IDRenameVisitor(c_ast.NodeVisitor):
         if node.args:
             self.visit(node.args)
 
-class FuncCallFindVisitor(c_ast.NodeVisitor):
-    def __init__(self, name):
-        self.name = name
-        self.argslist = []
-        self.containerlist = []
-
-    def visit_FuncCall(self, node): #avoids renaming calling library
-        if (node.name.name == self.name): #node.name AND node.args
-            self.containerlist.append( node )
-            self.argslist.append( copy.deepcopy(node.args.exprs) )
-        c_ast.NodeVisitor.generic_visit(self, node)
-
 
 class FuncDefVisitor(c_ast.NodeVisitor):
     def __init__(self, target):
@@ -59,21 +47,15 @@ class FuncDefVisitor(c_ast.NodeVisitor):
         return
 
 
-def getFunc_AST(filename, clientname, libname=None):
+def getFunc_AST(filename, funcname):
     # Note that cpp is used. Provide a path to your own cpp or
     # make sure one exists in PATH.
     ast = parse_file(filename, use_cpp=True, cpp_path='gcc',
                      cpp_args=['-E', r'-Iutils/fake_libc_include'])
 
-    clientv = FuncDefVisitor(clientname)
-    clientv.visit(ast)
-
-    if libname:
-        libv = FuncDefVisitor(libname)
-        libv.visit(ast)
-        lib_ast = libv.container
-        #lib_ast == None, when not found
-    return (clientv.container, lib_ast, ast)
+    v = FuncDefVisitor(funcname)
+    v.visit(ast)
+    return (v.container, ast)
 
 
 filename1 = "../../../../CC2_examples/neq/LoopMult5/old.c"
@@ -85,137 +67,51 @@ filename2 = "../../../../CC2_examples/neq/LoopMult5/new.c"
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--new', type=str, dest='new', default="new.c", help ="new source file", required=True)
-    parser.add_argument('--old', type=str, dest ='old', default="old.c", help="old source file", required=True)
-    parser.add_argument('--new2', type=str, dest='new2', default="new.c", help ="new source file", required=True)
-    parser.add_argument('--old2', type=str, dest ='old2', default="old.c", help="old source file", required=True)
+    parser.add_argument('--new', type=str, dest='new', default="new.c", help ="new source file" )
+    parser.add_argument('--old', type=str, dest ='old', default="old.c", help="old source file")
     parser.add_argument('--gen_new', type=str, dest='gen_new', default="new.c", help ="new generated" )
     parser.add_argument('--gen_old', type=str, dest ='gen_old', default="old.c", help="old generated")
     parser.add_argument('--client', type=str, dest ='client', default="client", help="client function name")
-    parser.add_argument('--lib', type=str, dest='lib', default="lib", help="lib function name")
-    parser.add_argument('--client2', type=str, dest ='client2', default="client", help="client2 function name")
-    parser.add_argument('--lib2', type=str, dest='lib2', default="lib", help="lib2 function name")
-    parser.add_argument('--useV2', action='store_true', default=False) #NOTE use V2 LIBRARIES, PASTE is still 1 before 2
-
+    #parser.add_argument('--lib', type=str, dest='lib', default="lib", help="lib function name")
 
     args = parser.parse_args()
     path_old = args.old
     path_new = args.new
-    path_old2 = args.old2
-    path_new2 = args.new2
     gen_old = args.gen_old
     gen_new = args.gen_new
     clientname = args.client
-    libname = args.lib
-    clientname2 = args.client2
-    libname2 = args.lib2
-    useV2 = args.useV2
-    
-    if not useV2:
-        client1n, lib1n, fulltree1n = getFunc_AST(path_new, clientname, libname)
-        client1o, lib1o, fulltree1o = getFunc_AST(path_old, clientname, libname)
-        client2n, _2, fulltree2n = getFunc_AST(path_new2, clientname2, libname2) #eventually something else
-        _1, _2, fulltree2o = getFunc_AST(path_old2, clientname2, libname2)
-        client_box = client1n
-        client_in = client2n
-    else:
-        client2n, _2, fulltree2n = getFunc_AST(path_new, clientname, libname)
-        _1, _2, fulltree2o = getFunc_AST(path_old, clientname, libname)
-        client1n, lib1n, fulltree1n = getFunc_AST(path_new2, clientname2, libname2) #eventually something else
-        client1o, lib1o, fulltree1o = getFunc_AST(path_old2, clientname2, libname2)
-        client_box = client1n
-        client_in = client2n        
-        """client_box = client2n #WRONG!
-        client_in = client1n"""
-        temp = libname
-        libname = libname2
-        libname2 = temp
-        #temp = clientname
-        #clientname = clientname2
-        #clientname2 = temp
+
+    client1, fulltree1 = getFunc_AST(path_new, clientname)
+    client2, fulltree2 = getFunc_AST(path_old, clientname) #eventually something else
     #check if returns
-    
-    boxCallVisitor = FuncCallFindVisitor(libname)
-    boxCallVisitor.visit(client_box)
-    assert(boxCallVisitor.containerlist[0] is not None)
-    args_box = boxCallVisitor.argslist[0]
-    boxCallName = boxCallVisitor.containerlist[0].name.name
+    if isinstance(client1.body.block_items[-1], c_ast.Return):
+        client1.body.block_items[-1] = client1.body.block_items[-1].expr
+    #if isinstance(client2.body.block_items[-1], c_ast.Return):
+    #    client2.body.block_items[-1] = client2.body.block_items[-1].expr
 
-    inCallVisitor = FuncCallFindVisitor(libname2)
-    inCallVisitor.visit(client_in)
-    assert(inCallVisitor.containerlist[0] is not None)
-    args_in_list = inCallVisitor.argslist
-    inFuncCall_list = inCallVisitor.containerlist
-    
-    for args_in, inFuncCall in zip(args_in_list, inFuncCall_list):
-        # TODO always replace client_in(lib_in) with client_in(lib_box), adjusting args to context
-        if len(args_box) > 2 or len(args_in) > 2 or len(args_box) <= 0 or len(args_in) <= 0:
-            print("Error: Lib call has > 2, or <= 0 args, more preprocessing needed")
-            return
-        inFuncCall.name.name = boxCallName
-        if len(args_box) == 1:
-            inFuncCall.args.exprs = inFuncCall.args.exprs[:1]
-        elif len(args_box) == 2 and len(args_in) == 2:
-            None
-        elif len(args_box) == 2 and len(args_in) == 1:
-            inFuncCall.args.exprs.extend(args_in)    
-    
-    if not useV2:
-        client1n_RetExpr = None
-        if isinstance(client1n.body.block_items[-1], c_ast.Return):
-            client1n_RetExpr = copy.deepcopy(client1n.body.block_items[-1].expr)
-            client1n.body.block_items[-1] = client1n.body.block_items[-1].expr
+    client2copy = copy.deepcopy(client2)
+    v = IDRenameVisitor("1")
+    v.visit(client2copy)
 
-        client2ncopy = copy.deepcopy(client2n)
-        v = IDRenameVisitor("1")
-        v.visit(client2ncopy)
-        
-        client2n_RetExpr = None
-        #TODO: Better INTERACTION between return values of merged benchmarks? (SUM right now)
-        if client1n_RetExpr and isinstance(client2ncopy.body.block_items[-1], c_ast.Return):
-            client2n_RetExpr = copy.deepcopy(client2ncopy.body.block_items[-1].expr)
-            client2ncopy.body.block_items[-1].expr = c_ast.BinaryOp(op='+',left= client1n_RetExpr, right=client2n_RetExpr )
-        
-        client1n.body.block_items.extend(client2ncopy.body.block_items)
-        client1n.decl.type.args.params.extend(client2ncopy.decl.type.args.params)
-    else:
-        client2n_RetExpr = None
-        if isinstance(client2n.body.block_items[-1], c_ast.Return):
-            client2n_RetExpr = copy.deepcopy(client2n.body.block_items[-1].expr)
-            client2n.body.block_items[-1] = client2n.body.block_items[-1].expr
-
-        client1ncopy = copy.deepcopy(client1n)
-        v = IDRenameVisitor("1")
-        v.visit(client1ncopy)
-
-        client1n_RetExpr = None
-        #TODO: Better INTERACTION between return values of merged benchmarks? (SUM right now)
-        if client2n_RetExpr and isinstance(client1ncopy.body.block_items[-1], c_ast.Return):
-            client1n_RetExpr = copy.deepcopy(client1ncopy.body.block_items[-1].expr)
-            client1ncopy.body.block_items[-1].expr = c_ast.BinaryOp(op='+',left= client2n_RetExpr, right=client1n_RetExpr )
-        
-        client2n.body.block_items.extend(client1ncopy.body.block_items)
-        client2n.decl.type.args.params.extend(client1ncopy.decl.type.args.params)
-        client1n.body = client2n.body
-        client1n.decl = client2n.decl
-    #client1o = client1n
-    
-    client1o.body = client1n.body
-    client1o.decl = client1n.decl
+    client1.body.block_items.extend(client2copy.body.block_items)
+    client1.decl.type.args.params.extend(client2copy.decl.type.args.params)
+    #client2 = client1
+    client2.body = client1.body
+    client2.decl = client1.decl
 
     generator = c_generator.CGenerator()
-    print(generator.visit(fulltree1n))
-    print(generator.visit(fulltree1o))
+    print(generator.visit(fulltree1))
+    print(generator.visit(fulltree2))
     #import pdb; pdb.set_trace()
 
     with open(gen_new, 'w+') as f:
-        f.write(generator.visit(fulltree1n))
+        f.write(generator.visit(fulltree1))
     with open(gen_old, 'w+') as f:
-        f.write(generator.visit(fulltree1o))
+        f.write(generator.visit(fulltree2))
 
     #import pdb; pdb.set_trace()
-    #client1n.body.block_items[-1].show()
-    #client1o.body.block_items[-1].show()
+    #client1.body.block_items[-1].show()
+    #client2.body.block_items[-1].show()
 
 # OK, we've seen that the top node is FileAST. This is always the
 # top node of the AST. Its children are "external declarations",
@@ -237,4 +133,4 @@ def main():
 #function_decl = ast.ext[2].decl
 
 if __name__ == "__main__":
-    main()
+main()
