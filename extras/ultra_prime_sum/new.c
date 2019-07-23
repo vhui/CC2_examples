7981af559997da6fc954d8facc3db15e792f0df9
Vincent Hui <vhui@alumni.ubc.ca>
2019-07-22 21:08:17 -0700

    another base instance

---
+++ extras/ultra_prime_sum/new.c
@@ -0,0 +1,23 @@
+int client(int n){
+    int i = 0;
+    int sum = 0;
+    while (i<=n){
+        if (lib(i) == 0 ){
+            sum +=i;
+        }
+        i++;
+    }
+    return (lib(sum) == 0);
+}
+
+int lib(int a){
+    int i = 2;
+    int count = 0;
+    while (i < a){
+        if ((a % i ) != 0){
+            count = 1;
+        }
+        i++;
+    }
+    return count;
+}
