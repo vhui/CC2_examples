05345b9a4c66411a6a4b466a492ae95bf6a4e18b
Vincent Hui <vhui@alumni.ubc.ca>
2019-07-24 13:35:14 -0700

    potential bugs from SeqMerge, without ground truth confirmation

---
+++ weirdInstances/EQ_oneBoundEQ_is_prime2/libA/new.c
@@ -0,0 +1,38 @@
+int lib(int x)
+{
+  if (x > 11)
+    return 11;
+  else
+    return x - 1;
+
+}
+
+int client(int x, unsigned int x_copy1)
+{
+  int ret;
+  if ((x < (-100)) || (x > 100))
+  {
+    ret = x;
+  }
+  else
+  {
+    if (x > lib(x))
+      ret = x;
+    else
+      ret = lib(x);
+
+  }
+
+  int ret_copy1;
+  if (x_copy1 < 19)
+  {
+    ret_copy1 = lib(20);
+  }
+  else
+  {
+    ret_copy1 = lib(x_copy1);
+  }
+
+  return ret + ret_copy1;
+}
+
