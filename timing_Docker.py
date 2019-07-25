2c36b69d56e362a0c7808c179e40ec5a3190410e
vhui <vincent_w_hui@hotmail.com>
2019-07-24 22:38:00 -0400

    fixed timer

--- timing_Docker.py
+++ timing_Docker.py
@@ -43,10 +43,10 @@ for dirpath, dnames, fnames in os.walk("./"):
     #print(dirpath)
     if len(dirpath.split("/")) != 4: #not inside gen folder!
         continue
-    if "mergedHard_CallMerge" not in dirpath:
+    if "mergedHard" not in dirpath:
         continue
-    #if "mergedHard_CallMerge" in dirpath:
-    #    continue
+    if "mergedHard_CallMerge" in dirpath:
+        continue
     if "EXTRA_prime_sum" in dirpath:
         continue
     #if "eq/" not in dirpath and "extras" not in dirpath:
@@ -381,7 +381,7 @@ for dirpath, dnames, fnames in os.walk("./"):
         CLEVER_TIME = TIMEOUT"""
 
     print("%-35s CC2-Hybrid: %-8s ,%-8.4f (Solve) ,,%-7.3f\tCC2-Concurrent: %-8s ,%-8.4f (Solve) ,,%-7.3f\tKleeCLEVER: %-8s ,%-8.4f" % (dirpath, CC2_RESULT, CC2_TIME, CC2_SOLVE_TIME, CC2_CONC_RESULT, CC2_CONC_TIME, CC2_CONC_SOLVE_TIME, KLEECLEVER_RESULT, KLEECLEVER_TIME))    
-    with open("timingJul20CallMerge.csv", 'a') as f:
+    with open("timingJul23SeqMerge.csv", 'a') as f:
         f.write("%-20s: %s,%-8.4f ; %s,%-8.4f ; %s,%-8.4f\n" %(dirpath, CC2_RESULT, CC2_TIME, CC2_CONC_RESULT, CC2_CONC_TIME, KLEECLEVER_RESULT, KLEECLEVER_TIME))
     if CC2_RESULT != KLEECLEVER_RESULT and CC2_RESULT != "timeout" and KLEECLEVER_RESULT != "timeout":
         print("Disagreement error: %s" % dirpath)
